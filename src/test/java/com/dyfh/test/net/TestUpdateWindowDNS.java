1df2cf14e38192539ebed97ee7f6dfa085f80561
谭吉湘 <tanjixiang@gooagoo.com>
2017-07-12 16:32:42 +0800

    加入junit

--- src/test/java/com/dyfh/test/net/TestUpdateWindowDNS.java
+++ src/test/java/com/dyfh/test/net/TestUpdateWindowDNS.java
@@ -1,9 +1,19 @@
 package com.dyfh.test.net;
 
+import org.junit.Test;
+
 import com.dyfh.net.UpdateWindowDNS;
 
 public class TestUpdateWindowDNS
 {
+    @Test
+    public static void testUpdateWindowDNS() throws Exception
+    {
+        String[] dnsArray = new String[] { "192.168.5.200", "8.8.8.8" };
+        UpdateWindowDNS.setWindowDNS(dnsArray);
+        System.out.println(UpdateWindowDNS.flushWindowDNS());
+    }
+
     public static void main(String[] args) throws Exception
     {
         String[] dnsArray = new String[] { "192.168.5.200", "8.8.8.8" };
