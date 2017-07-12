d05a319095af9982639b3cadfda0b165cc511879
谭吉湘 <tanjixiang@gooagoo.com>
2017-07-12 16:41:07 +0800

    加入断言

--- src/test/java/com/dyfh/test/net/TestUpdateWindowDNS.java
+++ src/test/java/com/dyfh/test/net/TestUpdateWindowDNS.java
@@ -1,5 +1,7 @@
 package com.dyfh.test.net;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import com.dyfh.net.UpdateWindowDNS;
@@ -7,11 +9,13 @@ import com.dyfh.net.UpdateWindowDNS;
 public class TestUpdateWindowDNS
 {
     @Test
-    public static void testUpdateWindowDNS() throws Exception
+    public void testUpdateWindowDNS() throws Exception
     {
         String[] dnsArray = new String[] { "192.168.5.200", "8.8.8.8" };
         UpdateWindowDNS.setWindowDNS(dnsArray);
-        System.out.println(UpdateWindowDNS.flushWindowDNS());
+        String flushResult = UpdateWindowDNS.flushWindowDNS();
+        System.out.println(flushResult);
+        assertEquals("执行成功", "WindowsIP配置已成功刷新DNS解析缓存。", flushResult);
     }
 
     public static void main(String[] args) throws Exception
